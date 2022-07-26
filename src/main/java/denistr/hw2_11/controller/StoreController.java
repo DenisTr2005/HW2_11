ab088056e43e33b952de7e50a014207717c03b76
Денис <den_2005@list.ru>
2022-07-26 15:25:52 +0300

    first

---
+++ src/main/java/denistr/hw2_11/controller/StoreController.java
@@ -0,0 +1,37 @@
+package denistr.hw2_11.controller;
+
+import denistr.hw2_11.data.Basket;
+import denistr.hw2_11.service.StoreService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.List;
+
+@RestController
+@RequestMapping("/order")
+public class StoreController {
+    private final StoreService storeService;
+    public StoreController(StoreService storeService) {
+        this.storeService = storeService;
+    }
+    @GetMapping("/add")
+    public List<Integer> add(@RequestParam("id") List<Integer> id) {
+        return storeService.add(id);
+    }
+    @GetMapping("/get")
+    public Basket get() {
+        return storeService.get();
+    }
+    @PostConstruct
+    public void init() {
+        System.out.println("создан бин StoreController");
+    }
+    @PreDestroy
+    public void destroy() {
+        System.out.println("удален бин StoreController");
+    }
+}
