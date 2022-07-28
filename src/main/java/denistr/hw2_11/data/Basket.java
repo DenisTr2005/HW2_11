c5650c2a59619a8cc0ebd4858a5cc442c33907f7
Денис <den_2005@list.ru>
2022-07-28 13:15:39 +0300

    commit 5

--- src/main/java/denistr/hw2_11/data/Basket.java
+++ src/main/java/denistr/hw2_11/data/Basket.java
@@ -1,8 +1,9 @@
 package denistr.hw2_11.data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.context.annotation.SessionScope;
 import java.util.List;
-@Repository
+@Component
 @SessionScope
 public class Basket {
     private final List<Integer> id;
